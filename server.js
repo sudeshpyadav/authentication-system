9f51fb7c035f53a9df52988a4a7639e8af9727a0
sudesh <sudesh.pal@bigbasket.com>
2019-11-18 14:08:27 +0530

    Added login module and api test cases

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
-
+app.use(express.json());
 const users = [];
 
 app.get('/users', (req, res) => {
     res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({name:req.body.name, password: hashedPassword});
+        res.status(201).send()
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find((user) => user.name === req.body.name);
+    if(user == null){
+        return res.status(400).send("This user is not registered");
+    }
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)){
+            return res.status(200).send("Success");
+        }
+        return res.status(400).send("Invalid username/password")
+    } catch (e) {
+        return res.status(500).send()
+    }
+
 });
 app.listen(3000);
 
--- test/test-user.js
+++ test/test-user.js
@@ -6,12 +6,38 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+describe('/POST users', () => {
+    it('It should save new user', (done) => {
+        data = {
+            name: "sudesh",
+            password: "password"
+        };
+        chai.request(server).post('/users').send(data).end((err, res) => {
+            res.should.have.status(201);
+            done();
+        });
+    })
+});
+
+describe('/POST users/login', () => {
+    it('It shold login the user', (done) => {
+        data = {
+            name: "sudesh",
+            password: "password"
+        };
+        chai.request(server).post('/users/login').send(data).end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    })
+});
+
 describe('/GET users', () => {
    it('It should get all the users', (done) => {
         chai.request(server).get('/users').end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('array');
-            res.body.length.should.be.eql(0);
+            res.body.length.should.be.eql(1);
             done()
         });
    })

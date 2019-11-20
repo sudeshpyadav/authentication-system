d9db48737ca04b751a19445c2032fc95896f7792
sudesh <sudesh.pal@bigbasket.com>
2019-11-20 17:53:14 +0530

    package-lock changes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('./models/user_model').User;
 const jwt = require('jsonwebtoken');
-
+const logger = require('./logger');
 const router = express.Router();
 
 
